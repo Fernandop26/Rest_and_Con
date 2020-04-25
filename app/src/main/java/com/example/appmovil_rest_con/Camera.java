3d3aad33786bf1f69766079672a5270f22189038
Miriam Morata Vegas <Miriam.Morata@e-msoffice.uab.cat>
2020-04-25 16:37:04 +0200

    boton camara + modelo + classificador

---
+++ app/src/main/java/com/example/appmovil_rest_con/Camera.java
@@ -0,0 +1,109 @@
+package com.example.appmovil_rest_con;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class Camera extends AppCompatActivity {
+
+    private Button openCamera;
+
+    // for permission requests
+    public static final int REQUEST_PERMISSION = 300;
+
+    // request code for permission requests to the os for image
+    public static final int REQUEST_IMAGE = 100;
+
+    // will hold uri of image obtained from camera
+    private Uri imageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_model);
+
+        // request permission to use the camera on the user's phone
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+        }
+
+        // request permission to write data (aka images) to the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        // request permission to read data (aka images) from the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        // on click for inception float model
+        openCamera = (Button)findViewById(R.id.cameraButton);
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open camera
+                openCameraIntent();
+            }
+        });
+
+    }
+
+    // opens camera for user
+    private void openCameraIntent(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    // checks that the user has allowed all the required permission of read and write and camera. If not, notify the user and close the application
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(),"This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG);
+                System.exit(0);
+            }
+        }
+    }
+
+    // dictates what to do after the user takes an image, selects and image, or crops an image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+       if(resultCode == RESULT_OK){
+            Intent i = new Intent(Camera.this, Classify.class);
+            // put image data in extras to send
+            i.putExtra("resID_uri", imageUri);
+            startActivity(i);
+        }
+    }
+}
